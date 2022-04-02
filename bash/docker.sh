0b298d3634b2f23857d346849d00db3646c87488
Sergey Sorokin <algademius@gmail.com>
2022-04-02 20:25:20 +0300

    Update docker.sh

--- bash/docker.sh
+++ bash/docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Usage:
-# alias docker='bash path/to/docker.sh docker'
+#   alias docker='bash path/to/docker.sh docker'
 if [[ "$1" == "docker" ]]; then
   if [[ "$2" == "ps" || "$2" == "images" ]]; then
     "$@" | docker-color-output
